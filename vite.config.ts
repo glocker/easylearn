c793fa30e7a1689a4109d51eeede222e4f25e314
glocker <georgeglocker@gmail.com>
2025-03-28 17:20:53 +0100

    Added tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
   plugins: [
     react(),
+    tailwindcss(),
     VitePWA({
       registerType: "autoUpdate",
       includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
@@ -35,7 +37,7 @@ export default defineConfig({
     }),
   ],
   server: {
-    port: 3000,
+    port: 1024,
   },
   build: {
     sourcemap: true,
