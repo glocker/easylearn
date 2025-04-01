11c1e910ce661c483b1cd45bb569d23ab229d0d0
glocker <georgeglocker@gmail.com>
2025-04-01 12:51:24 +0200

    Added event listeners for debugging

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,1 +1,14 @@
+// Console logs for debugging
 console.log("log from service worker");
+
+addEventListener("install", () => {
+  console.log("Service worker is installing...");
+});
+
+addEventListener("activate", () => {
+  console.log("Service worker is activated...");
+});
+
+addEventListener("fetch", () => {
+  console.log("Service worker is fetching...");
+});
