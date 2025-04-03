c809ef3d13fafe9c4cf61b8f9ecf7ae5e90bd9b9
glocker <georgeglocker@gmail.com>
2025-04-03 12:17:19 +0200

    Retrieve cache if request matches

--- serviceWorker.js
+++ serviceWorker.js
@@ -16,15 +16,23 @@ addEventListener("fetch", (fetchEvent) => {
 
   if (fetchEvent) {
     fetchEvent.respondWith(
-      fetch(request)
-        .then((fetchResponse) => fetchResponse)
-        .catch((error) => {
-          return new Response("<h1>Oops!</h1><p>Failure to fetch data</p>", {
-            headers: {
-              "Content-type": "text/html; charset=utf-8",
-            },
+      caches.match(request).then((fetchResponse) => {
+        // Use cached resource if it's exists
+        if (fetchResponse) {
+          return fetchResponse;
+        }
+
+        // No cache - use regular fetching
+        return fetch(request)
+          .then((fetchResponse) => fetchResponse)
+          .catch((error) => {
+            return new Response("<h1>Oops!</h1><p>Failure to fetch data</p>", {
+              headers: {
+                "Content-type": "text/html; charset=utf-8",
+              },
+            });
           });
-        })
+      })
     );
   }
 });
